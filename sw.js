b28a9c950b576fb1dbc8a29e989202de70480ddb
WindHouse <155715318+PingPlayVN@users.noreply.github.com>
2026-03-03 06:46:07 +0700

    Update service worker cache version to v11.0

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // sw.js - Service Worker
 
-const CACHE_NAME = 'wind-share-v9.4'; // Đổi tên này nếu bạn update code để ép trình duyệt tải lại
+const CACHE_NAME = 'wind-share-v11.0'; // Đổi tên này nếu bạn update code để ép trình duyệt tải lại
 const ASSETS_TO_CACHE = [
     './',
     './index.html',
@@ -75,4 +75,5 @@ self.addEventListener('message', (event) => {
         self.skipWaiting();
     }
 });
+
 
