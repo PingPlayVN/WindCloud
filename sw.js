f01acda719d320e555ae8b9e4537f827265f193f
WindHouse <155715318+PingPlayVN@users.noreply.github.com>
2026-03-06 00:28:06 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // sw.js - Service Worker
 
-const CACHE_NAME = 'wind-share-v11.10'; // Đổi tên này nếu bạn update code để ép trình duyệt tải lại
+const CACHE_NAME = 'wind-share-v11.11'; // Đổi tên này nếu bạn update code để ép trình duyệt tải lại
 const ASSETS_TO_CACHE = [
     './',
     './index.html',
@@ -83,7 +83,6 @@ self.addEventListener('message', (event) => {
         self.skipWaiting();
     }
 });
-
 
 
 
