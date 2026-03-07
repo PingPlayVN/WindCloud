e4314348bb7d852453d830f20b7d0582c5b54b6b
WindHouse <155715318+PingPlayVN@users.noreply.github.com>
2026-03-07 11:30:20 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 
 
 const CacheManager = (() => {
-    const CACHE_NAME = 'wind-share-v11.18';
+    const CACHE_NAME = 'wind-share-v11.19';
     const ASSETS_TO_CACHE = [
         './',
         './index.html',
