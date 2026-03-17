5546fe47efdf9553f54b2e3f47db0f2be89a48c4
Trịnh Gia Phong <trinhgiaphong2k9@gmail.com>
2026-03-17 17:18:35 +0700

    update

--- gitpush.bat
+++ gitpush.bat
@@ -1,7 +1,7 @@
 @echo off
 cd /d "%~dp0"
 git add .
-git commit -m "update animation UI Vocab Checker SlideBar"
+git commit -m "update "
 git push
 workbox injectManifest workbox-config.js
 echo Done!
